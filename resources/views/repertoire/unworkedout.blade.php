8fd859141f83530df3426f33a955539e1aa9a35f
Donald KANTI <donaldkant2000@gmail.com>
2021-10-04 21:22:14 +0100

    Added option to directly stream songs on the website

--- resources/views/repertoire/unworkedout.blade.php
+++ resources/views/repertoire/unworkedout.blade.php
@@ -1,14 +1,88 @@
 @extends('layouts.app')
 
-@push('styles')
-<link rel="stylesheet" href="{{ asset('css/audio.css') }}" />
-@endpush
-
 @section('content')
 <div class="container">
     <div class="row">
         @if ($songs->count())
         @foreach ($songs as $song)
+        <div class="col-md-4">
+            <div class="card">
+                <div class="songs">
+                    <div class="song active">
+                        <div class="thumbnail"><img class="img rounded" src="{{ asset('images/headphone.png') }}" /></div>
+                        <h1 class="title">{{ $song->title }}</h1>
+                        <p class="description">{{ $song->description }}</p>
+                        <p class="description">Par : {{ $song->author }}</p>
+                    </div>
+                </div>
+                <div class="text-center"><a class="btn" href="{{ route('song_download', $song) }}">TÉLÉCHARGER</a></div>
+                <div class="container-audio">
+                    <audio controls>
+                        <source src="{{ $song->file }}">
+                        Your browser does not Support the audio Tag
+                    </audio>
+                </div>
+                @auth
+                @if (count(auth()->user()->roles) == 2)
+                <div class="text-center mt-4">
+                    <form action="{{ route('mark_as_workedout', $song) }}">
+                        <button class="btn text-light rounded-2 btn-success">
+                            Marquer Copiée
+                        </button>
+                    </form>
+                </div>
+                <div class="text-center mt-4">
+                    <form action="{{ route('song_delete', $song) }}">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn text-light rounded-2 btn-danger">
+                            Supprimer le morceau
+                        </button>
+                    </form>
+                </div>
+                @endif
+                @endauth
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="songs">
+                    <div class="song active">
+                        <div class="thumbnail"><img class="img rounded" src="{{ asset('images/headphone.png') }}" /></div>
+                        <h1 class="title">{{ $song->title }}</h1>
+                        <p class="description">{{ $song->description }}</p>
+                        <p class="description">Par : {{ $song->author }}</p>
+                    </div>
+                </div>
+                <div class="text-center"><a class="btn" href="{{ route('song_download', $song) }}">TÉLÉCHARGER</a></div>
+                <div class="container-audio">
+                    <audio controls>
+                        <source src="{{ $song->file }}">
+                        Your browser does not Support the audio Tag
+                    </audio>
+                </div>
+                @auth
+                @if (count(auth()->user()->roles) == 2)
+                <div class="text-center mt-4">
+                    <form action="{{ route('mark_as_workedout', $song) }}">
+                        <button class="btn text-light rounded-2 btn-success">
+                            Marquer Copiée
+                        </button>
+                    </form>
+                </div>
+                <div class="text-center mt-4">
+                    <form action="{{ route('song_delete', $song) }}">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn text-light rounded-2 btn-danger">
+                            Supprimer le morceau
+                        </button>
+                    </form>
+                </div>
+                @endif
+                @endauth
+            </div>
+        </div>
         <div class="col-md-4">
             <div class="card">
                 <div class="songs">
